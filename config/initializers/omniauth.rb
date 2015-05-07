853c16b46c0fc3e43f799d87ca19198b1e26ca33
ncronquist <nick@ncronquist.com>
2015-05-06 21:47:36 -0700

    Add missing comma from omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,7 +4,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'] {
     access_type: 'offline',
     # scope: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/calendar',
-    scope: 'userinfo.email calendar'
+    scope: 'userinfo.email calendar',
     redirect_uri:'http://localhost/auth/google_oauth2/callback'
   }
 end
