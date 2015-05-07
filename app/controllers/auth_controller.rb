6c314de7e98456956026d529bd307889e15e27b6
ncronquist <nick@ncronquist.com>
2015-05-06 22:27:44 -0700

    Add auth controller template and basic events index.html.erb page

---
+++ app/controllers/auth_controller.rb
@@ -0,0 +1,31 @@
+class AuthController < ApplicationController
+
+  def callback
+    provider_user = request.env['omniauth.auth']
+    render json: provider_user
+
+    #find create a user
+    # user = User.find_or_create_by(provider_id: provider_user['uid'], provider: params[:provider]) do |u|
+    #   u.provider_hash = provider_user['credentials']['token']
+    #   u.name = provider_user['info']['name']
+    #   u.email = provider_user['info']['email']
+    # end
+
+    # #create user session
+    # session[:user_id] = user.id
+
+    # #send them home
+    # redirect_to root_path
+  end
+
+  def logout
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+
+  def failure
+    #TODO: display error page
+    render plain: "this is a failure"
+  end
+
+end
