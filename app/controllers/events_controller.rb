10123617b2d04dbf955f5d4e4d9b87ea04cf337b
ncronquist <nick@ncronquist.com>
2015-05-10 00:02:27 -0700

    Cleanup weekly repeating event adding

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -15,26 +15,24 @@ class EventsController < ApplicationController
   end
 
   def create
-    # render :json => params
+  #   render :json => params
 
-    # def create_event(start_dtm,
-    #                 end_dtm,
-    #                 classgroup_id,
-    #                 street,
-    #                 city,
-    #                 state,
-    #                 zip)
     def create_event(start_dtm,
                     end_dtm,
-                    classgroup_id)
+                    classgroup_id,
+                    street,
+                    city,
+                    state,
+                    zip)
+
       # Create new event
       event = Event.new
       event.start = start_dtm
       event.end = end_dtm
-      # event.street = street
-      # event.city = city
-      # event.state = state
-      # event.zip = zip
+      event.street_address = street
+      event.city = city
+      event.state = state
+      event.zip = zip
       event.save
 
       # Associate event to classgroup
@@ -71,31 +69,36 @@ class EventsController < ApplicationController
     end_dtm = DateTime.strptime(params[:event][:end] + ' ' + params[:UTC],
                                 '%m/%d/%Y %H:%M %p %z')
     classgroup_id = params[:event][:classgroup_id]
+    street = params[:event][:street]
+    city = params[:event][:city]
+    state = params[:event][:state]
+    zip = params[:event][:zip]
+    repeat = params[:event][:repeat][:repeat]
 
-    if !params[:repeat][:repeat]
+    if !repeat
       # Not a repeating event
       # Add single instance of the event
       create_event(start_dtm, end_dtm, classgroup_id)
-
     else
       # Repeating event
       # Add multiple instances of the event
 
       # Get variables
-      reoccurrence_type = params[:repeat][:reoccurrence_type]
-      reoccurrence_period = params[:repeat][:reoccurrence_period].to_i
-      first_event_start = DateTime.strptime(params[:event][:start] + ' ' + params[:UTC], '%m/%d/%Y %H:%M %p %z')
-      first_event_end = DateTime.strptime(params[:event][:end] + ' ' + params[:UTC], '%m/%d/%Y %H:%M %p %z')
+      reoccurrence_type = params[:event][:repeat][:reoccurrence_type]
+      reoccurrence_period = params[:event][:repeat][:reoccurrence_period].to_i
+      first_event_start = DateTime.strptime(params[:event][:start] + ' ' +
+                                            params[:UTC],
+                                            '%m/%d/%Y %H:%M %p %z')
+      first_event_end = DateTime.strptime(params[:event][:end] + ' ' +
+                                          params[:UTC],
+                                          '%m/%d/%Y %H:%M %p %z')
       days = []
-      params[:repeat][:days].each do |k,v|
+      params[:event][:repeat][:days].each do |k,v|
         days << v.to_i
       end
-      end_mode = params[:repeat][:until]
-      if end_mode == "repeat_times"
-        occurrences = params[:repeat][:until_times].to_i
-      else
-        last_event = Date.strptime(params[:repeat][:until_end], '%m/%d/%Y')
-      end
+      occurrences = (params[:event][:repeat][:occurrences] || '9999999999').to_i
+      last_event = Date.strptime(params[:event][:repeat][:end_date] ||
+                                        '12/31/9999', '%m/%d/%Y')
 
       # eventObj = {}
       # eventObj['reoccurrence_type'] = reoccurrence_type
@@ -103,23 +106,18 @@ class EventsController < ApplicationController
       # eventObj['first_event_start'] = first_event_start
       # eventObj['first_event_end'] = first_event_end
       # eventObj['days'] = days
-      # eventObj['end_mode'] = end_mode
-      # eventObj['occurrences'] = occurrences || 0
-      # eventObj['last_event'] = last_event || Date.new(1900,1,1)
-
+      # eventObj['occurrences'] = occurrences
+      # eventObj['last_event'] = last_event
       # render :json => eventObj
 
       event_start = first_event_start
       event_end = first_event_end
-      # first_event = Date.strptime(event_start, '%m/%d/%Y')
-      occurrences = occurrences || 9999999999
-      last_event = last_event || Date.new(9999,12,31)
       events_created = 0
       week = 1
 
       loop do
         days.each_with_index do |day,idx|
-          # If it's teh first week and the day is less than the event start day,
+          # If it's the first week and the day is less than the event start day,
           # skip to the next day
           next if week == 1 && day < event_start.wday
 
@@ -127,9 +125,15 @@ class EventsController < ApplicationController
           # already created enough occurences, break out of the loop
           break if event_start > last_event + 1.day || events_created >= occurrences
 
-          create_event(event_start, event_end, classgroup_id)
+          create_event(event_start,
+                      event_end,
+                      classgroup_id,
+                      street,
+                      city,
+                      state,
+                      zip)
 
-          # Update the never of events created
+          # Update the number of events created
           events_created += 1
 
           # Update the next event_start date
@@ -182,33 +186,28 @@ end
 ###########################################################################
 # {
 #   utf8: "✓",
-#   authenticity_token: "DdFqQ08qnIetmob8aJW89H3K8bdHiVZmdrl448FE2Xm1Z4edA+9ZN8G8vnX2TieTTtJXnk0ssZ3wFgIIfXR7Mg==",
-#   event:  {
-#             classgroup_id: "1",
-#             start: "05/11/2015 5:30 PM",
-#             end: "05/11/2015 6:30 PM"
-#           },
-#   repeat: {
-#             repeat: "true",
-#             freqtype: "Weekly",
-#             freq: "1",
-#             days: {
-#                     sun: "0",
-#                     mon: "1",
-#                     tue: "2",
-#                     wed: "3",
-#                     thu: "4",
-#                     fri: "5",
-#                     sat: "6"
-#                   },
-#             until: "repeat_times",
-#             until_times: "28"
-#           },
+#   authenticity_token: "BjAAtDHZ2yZ4WrA/baDuy45cPI5f6LEnlrkajrTS98S+hu1qfRwelhR8iLbze3WsvUSap1VNVtwQFmBlCOJVjw==",
+#   event: {
+#     classgroup_id: "1",
+#     street: "225 32nd Ave E",
+#     state: "WA",
+#     zip: "98112",
+#     start: "05/18/2015 7:52 PM",
+#     end: "05/18/2015 8:52 PM",
+#     repeat: {
+#       repeat: "true",
+#       reoccurrence_type: "week",
+#       reoccurrence_period: "1",
+#       days: {
+#         mon: "1",
+#         tue: "2"
+#       },
+#       end_mode: "end_date",
+#       end_date: "06/29/2015"
+#     }
+#   },
+#   UTC: "-07:00",
 #   commit: "Save Event",
 #   controller: "events",
 #   action: "create"
-#
-#   OTHER OPTIONS
-#   until: "repeat_end",
-#   until_end: "08/26/2015"
 # }
