a80a3a055a44750d6f70a81c6a06e240608c71b3
ncronquist <nick@ncronquist.com>
2015-05-10 14:55:01 -0700

    Update auth to save google information to user model

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -4,27 +4,16 @@ class AuthController < ApplicationController
 
   def callback
     provider_user = request.env['omniauth.auth']
-    render json: provider_user
-
-    ### NOTES
-      #find create a user
-      # user = User.find_or_create_by(provider_id: provider_user['uid'], provider: params[:provider]) do |u|
-      #   u.provider_hash = provider_user['credentials']['token']
-      #   u.name = provider_user['info']['name']
-      #   u.email = provider_user['info']['email']
-      # end
-
-      # #create user session
-      # session[:user_id] = user.id
-
-      # #send them home
-      # redirect_to root_path
+    # render json: provider_user
 
     # Add Google credentials to user
-    # @current_user.provider = params[:provider]
-    # @current_user.provider_id = provider_user['uid']
-    # @current_user.provider_hash = provider_user['credentials']['token']
-    # @current_user.save
+    @current_user.provider = params[:provider]
+    @current_user.provider_id = provider_user['uid']
+    @current_user.provider_hash = provider_user['credentials']['token']
+    @current_user.save
+
+    flash[:info] = "Schedulacity is now connected with your Google account"
+    redirect_to events_path
 
   end
 
