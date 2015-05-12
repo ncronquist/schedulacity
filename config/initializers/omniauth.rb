5d079f8eaa1bb3fbf15899a4275ffb9e141f7812
ncronquist <nick@ncronquist.com>
2015-05-12 12:33:07 -0700

    Update omniauth to not include callback url

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {
     access_type: 'offline',
-    scope: 'userinfo.email calendar',
-    redirect_uri: request.base_url + '/auth/google_oauth2/callback'
+    scope: 'userinfo.email calendar'
   }
 end
